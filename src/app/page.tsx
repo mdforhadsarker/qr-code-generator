835d759583a1e659a3c5f00ae4da12f7807d6089
Forhad Sarkar <ashadullahalforhad@gmail.com>
2024-09-04 12:22:26 +0600

    initiate the repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,84 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useRef } from "react";
+import QRCode from "react-qr-code";
+import { saveAs } from "file-saver";
+import { toPng } from "html-to-image";
 
-export default function Home() {
+// Define the type for the ref
+type QRCodeRef = HTMLDivElement | null;
+
+const QrCodeGenerator: React.FC = () => {
+  // Define the state types
+  const [qrCode, setQrCode] = useState<string>("");
+  const [input, setInput] = useState<string>("");
+
+  // Use the ref with the specified type
+  const qrRef = useRef<QRCodeRef>(null);
+
+  const handleQrCodeGenerator = () => {
+    setQrCode(input);
+    setInput("");
+  };
+
+  const handleDownload = async () => {
+    if (qrRef.current) {
+      const dataUrl = await toPng(qrRef.current);
+      saveAs(dataUrl, "qr-code.png");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg p-8">
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          QR Code Generator
+        </h1>
+        <div className="flex flex-col sm:flex-row items-center p-4 gap-4">
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            name="qr-code"
+            placeholder="Enter your link"
+            className="border-2 rounded-md p-2 w-full sm:w-auto"
+            value={input}
+          />
+          <button
+            disabled={!input.trim()}
+            onClick={handleQrCodeGenerator}
+            className="p-2 bg-slate-500 rounded-md cursor-pointer hover:bg-slate-600 hover:text-white transition-colors w-full sm:w-auto"
+          >
+            Generate
+          </button>
+          <button
+            onClick={handleDownload}
+            disabled={!qrCode}
+            className="p-2 bg-green-500 rounded-md cursor-pointer hover:bg-green-600 hover:text-white transition-colors w-full sm:w-auto"
+          >
+            Download QR Code
+          </button>
+        </div>
+        {qrCode && (
+          <div ref={qrRef} className="mt-6 p-10 bg-white border rounded-md ">
+            <QRCode value={qrCode} size={400} bgColor="#fff" />
+          </div>
+        )}
+      </div>
+      <footer className="mt-8 text-center text-gray-600">
+        <p>
+          Developed by{" "}
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://github.com/mdforhadsarker"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Forhad
           </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          . All rights reserved.
+        </p>
+      </footer>
+    </div>
   );
-}
+};
+
+export default QrCodeGenerator;
